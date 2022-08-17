19de07fc6c350e885bf2056acb73298f648b5023
Jorge Vargas <eduardo.borisq@gmail.com>
2022-08-17 18:34:58 -0500

    Test for Game class
    We make the tests for the methods play_game, first_roll and second_roll
    
    Co-authored-by: jhanca27 <jhancarlohigueracaravajal@gmail.com>
    Co-authored-by: Brandon2002 <btapia@ucol.mx>
    Co-authoted-by: AlfredoPazZamora <apaz0@ucol.mx>

--- app/bowling.rb
+++ app/bowling.rb
@@ -42,6 +42,7 @@ class Game
 
             
         end
+        @rolls
     end
 
 
---
+++ app/bowling_spec.rb
@@ -0,0 +1,23 @@
+require 'rspec'
+require_relative './bowling'
+
+describe Game do
+  describe '.first_roll' do
+    it 'Print a random number' do
+      expect(Game.new.first_roll).to be_a_kind_of(Numeric)
+    end
+    it 'Random number 0..10' do
+      expect(Game.new.first_roll).to be_between(0, 10).inclusive
+    end
+  end
+  describe '.second_roll' do
+    it 'The second roll of a game' do
+      expect(Game.new.second_roll(10)).to be_a_kind_of(Numeric)
+    end
+  end
+  describe '.play_game' do
+    it 'Create an Array with the number of shots' do
+      expect(Game.new.play_game).to be_a_kind_of(Array)
+    end
+  end
+end
