21a77604adde1d0bf660b4945cea5b140fb7c1f9
“[jhanca27]” <[jhancarlohigueracarvajal@gmail.com]>
2022-08-18 19:08:54 -0500

    Add method rules, next_roll, last_game
    feat: add new method rules about game, next_roll does new roll and last_game extra ball to complete the frame
    Co-authored-by: Brandon2002 <btapia@ucol.mx>
    Co-authored-by: jorgeboris <eduardo.borisq@gmail.com>

--- app/bowling.rb
+++ app/bowling.rb
@@ -5,9 +5,9 @@ class Board_bowling
 end
 
 # Rules for spare
-class Spare
+#class Spare
 
-end
+#end
 
 # Rules for strike 
 class Strike
@@ -25,17 +25,27 @@ class Game
     def initialize
         @score = 0
         @rolls = []
+        @first_roll = first_roll
+        second_roll(@first_roll)
         play_game
         puts "Rolls : #{@rolls}"
     end
 
     def play_game
 
-        while @rolls.length < 20
-            @first_roll = first_roll
+        while @rolls.length < 29
             @rolls.push(@first_roll)
-            @rolls.push(second_roll(@first_roll))
+            
+            @rolls.push(@second_roll)
             @score += @first_roll + @second_roll
+            
+            if @rolls.length == 29 && (@first_roll == 10 || @first_roll + @second_roll == 10)
+                last_game
+                p "you have a bonus roll"
+            else
+            rules
+            end
+
             @rolls.push(@score)
         end
         @rolls
@@ -44,11 +54,40 @@ class Game
     def first_roll
         @first_roll = rand(0..10)
     end
+
     def second_roll (pins)    
         mod = 10 - pins
         @second_roll = rand(0..mod)
     end
 
+    def next_roll
+        @first_roll = first_roll
+        @second_roll = second_roll(@first_roll)
+    end
+
+    def last_game
+        @first_roll = first_roll
+        @score += @first_roll
+        @rolls.push(@first_roll)
+        
+    end
+
+    def rules
+        @score += if @first_roll == 10
+                next_roll
+                @first_roll + @second_roll
+        elsif @first_roll + @second_roll == 10 && @first_roll != 10
+                next_roll
+                @first_roll
+        else
+                next_roll
+                return 0
+        end
+    
+    end
+
+
+    
 
 end
 
